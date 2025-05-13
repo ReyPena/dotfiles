c786d06a88e601b445b46d2251eb567194072c02
ReyPena <zonickblack@gmail.com>
2025-05-12 20:42:33 -0600

    update config

---
+++ link-conf.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s $HOME/dotfiles/.config/paru $HOME/.config/paru
+ln -s $HOME/dotfiles/.config/wezterm $HOME/.config/wezterm
+ln -s $HOME/dotfiles/.config/gtk-4.0/* $HOME/.config/gtk-4.0
+ln -s $HOME/dotfiles/.config/gtk-3.0/* $HOME/.config/gtk-3.0
+
